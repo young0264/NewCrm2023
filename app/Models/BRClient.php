2947ade66ab35b172ed47e7291d55f7af62a08e1
young0264 <ny2485@naver.com>
2023-07-06 16:39:56 +0900

    mysql model 추가

--- app/Models/BRClient.php
+++ app/Models/BRClient.php
@@ -6,18 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-class BR_Client extends Model {
+class BRClient extends Model {
     use HasFactory;
     private static $oracle_table = "t_shop";
     private static $mysql_table = "nctViewCompanyClient";
 
-    public static function list($wheres="", $binds=[]) {
+    public static function list($wheres="", $bindings=[]) {
 
         $query = "select * from nctViewCompanyClient
                     where client_id is not null
                     {$wheres}
                     limit 10";
 
-        return DB::connection('br')->select($query,$binds);
+        return DB::connection('br')->select($query, $bindings);
     }
 }
