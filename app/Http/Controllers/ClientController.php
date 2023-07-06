4278b7322b0adce7d0d967fde29306ddbc3e76f3
young0264 <ny2485@naver.com>
2023-07-06 16:38:55 +0900

    고객검색 리스트 api 작성

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -1,16 +1,43 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\Bill_NEY;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use App\Service\ClientService;
 
-class ChargeController extends BaseController{
+class ClientController extends BaseController{
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    protected ClientService $clientService;
+
+    public function __construct(ClientService $clientService){
+        $this->clientService = $clientService;
+    }
+
+    public function getStoreInfo(Request $request) {
+        /**
+         * 검색어가 없을 경우 start
+         */
+        if (!$request->filled("sch_val")) {
+            print_r("empty");
+            return response()->json(
+                array(
+                    "status"=>"empty"
+                )
+            );
+        }
+
+        $items = $this->clientService->getClientStoreList($request->input());
+
+        return response()->json(
+            array(
+                "status"=>"ok",
+                "result"=>json_encode($items)
+            )
+        );
+    }
 
 
     public static function chargeNonMemberRegist(){
