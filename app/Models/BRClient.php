6002976f7342db69211948616c3cd89818a18abe
young0264 <ny2485@naver.com>
2023-07-10 11:53:14 +0900

    mysql,oracle findById 추가

--- app/Models/BRClient.php
+++ app/Models/BRClient.php
@@ -8,16 +8,21 @@ use Illuminate\Support\Facades\DB;
 
 class BRClient extends Model {
     use HasFactory;
-    private static $oracle_table = "t_shop";
     private static $mysql_table = "nctViewCompanyClient";
 
     public static function list($wheres="", $bindings=[]) {
-
-        $query = "select * from nctViewCompanyClient
-                    where client_id is not null
-                    {$wheres}
-                    limit 10";
-
+        $query = "select *
+                    from ".self::$mysql_table."
+                        where client_id is not null
+                            {$wheres}
+                            limit 10";
         return DB::connection('br')->select($query, $bindings);
+    }
+
+    public static function findByClientId($client_id, $group_site) {
+        $query = "select *
+                    from ".self::$mysql_table."
+                        where client_id = :client_id and group_site = :group_site";
+        return DB::connection('br')->select($query, ['client_id'=>$client_id, 'group_site'=>$group_site]);
     }
 }
--- app/Models/SCShop.php
+++ app/Models/SCShop.php
@@ -11,7 +11,6 @@ class SCShop extends Model{
     use HasFactory;
 
     public static function list($wheres='', $bindings=[]) {
-
         $query = "select * from (
                         select
                             (select f_shopname from t_shop where f_shopid = t1.f_bizid) as f_bizname,
@@ -21,6 +20,18 @@ class SCShop extends Model{
                     where f_shopid is not null
                     {$wheres}
                     and rownum < 10";
+
+        return DB::connection('sc')->select($query, $bindings);
+    }
+
+    public static function findByLoginId($f_loginid,$f_company) {
+        $query = "select * from (
+                        select
+                        (select f_shopname from t_shop where f_shopid = t1.f_bizid) as f_bizname,
+                        t1.*
+                            from t_shop t1)
+                                where f_loginid = :f_loginid and f_company = :f_company";
+        $bindings = ['f_loginid'=>$f_loginid,'f_company'=>$f_company];
 
         return DB::connection('sc')->select($query, $bindings);
     }
