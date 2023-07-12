a995682a786f8d117bb81aeccf3d34ec39acc4e4
young0264 <ny2485@naver.com>
2023-07-12 10:07:55 +0900

    cnt 상수로 변경

--- resources/views/charge/chargeRegistView.blade.php
+++ resources/views/charge/chargeRegistView.blade.php
@@ -32,8 +32,8 @@
                 console.log(this);
                 $(this).val($(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/, "$1-$2-$3").replace("--", "-"));
                 var pattern = /^(?:\d{3}-\d{3,4}-\d{4}|\d{2,3}-\d{3,4}-\d{4})$/;
-                const mobileCnt = 2;
-                for(let i = 1; i <= mobileCnt; i++) {
+                const MOBILE_CNT = 2;
+                for(let i = 1; i <= MOBILE_CNT; i++) {
                     if (document.getElementById("f_mobile"+i).value.match(pattern)) {
                         document.getElementById("f_mobile"+i).style.borderColor = "blue";
                     } else {
@@ -46,8 +46,8 @@
         email: function () {
             $(document).on("keyup", ".email", function () {
                 var pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-                const emailCnt = 2;
-                for(let i = 1; i <= emailCnt; i++) {
+                const EMAIL_CNT = 2;
+                for(let i = 1; i <= EMAIL_CNT; i++) {
                     if (document.getElementById("f_email"+i).value.match(pattern)) {
                         document.getElementById("f_email"+i).style.borderColor = "blue";
                     } else {
