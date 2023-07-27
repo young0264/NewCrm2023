5b17d6fb2029ce5e24e7824e2ffe67fda5144f15
young0264 <ny2485@naver.com>
2023-07-27 17:52:50 +0900

    publish excel export 파일 작성

--- app/Exports/PublishExport.php
+++ app/Exports/PublishExport.php
@@ -7,7 +7,7 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
 //print("<meta http-equiv=\"Content-Type\" content=\"application/vnd.ms-excel; charset=utf-8\">");
 
-class BillExport implements FromCollection, WithHeadings, WithColumnWidths, WithStyles {
+class PublishExport implements FromCollection, WithHeadings, WithColumnWidths, WithStyles {
 
     private $headers = ['f_bizname', 'f_shopname', 'f_price', 'f_pay_type', 'f_pay_interval', 'f_history', 'f_reply'
         , 'f_statement', 'f_tax_bill', 'f_issuedate'
@@ -15,6 +15,11 @@ class BillExport implements FromCollection, WithHeadings, WithColumnWidths, With
         , 'f_addr', 'f_business', 'f_event', 'f_public_addr1', 'f_public_addr2'
         ,'f_name1', 'f_mobile1', 'f_email1', 'f_name2', 'f_mobile2', 'f_email2'
         ,'f_product1', 'f_price1', 'f_product2', 'f_price2', 'f_product3', 'f_price3', 'f_product4', 'f_price4'
+    ];
+    private $headers_ko = ['본사명', '고객명', '공급가액', '결제방식', '결제주기', '내역', '회신', '거래명세서', '세금계산서', '발행날짜'
+        ,'사업자번호', '종사업장', '상호명', '대표자명', '주소', '업태', '종목', '발행주소1', '발행주소2'
+        ,'담당자1', '연락처1', '담당자메일', '담당자2', '연락처2', '담당자메일2'
+        ,'품목1', '공급가액1', '품목2', '공급가액2', '품목3', '공급가액3', '품목4', '공급가액4'
     ];
     private $items;
     private $params;
@@ -29,9 +34,6 @@ class BillExport implements FromCollection, WithHeadings, WithColumnWidths, With
         $res = array();
         foreach ($this->items as $item) {
             $arr_tmp = array();
-//            echo "<pre>";
-//            print_r($this->items);
-//            exit;
             foreach ($this->headers as $header) {
                 if (property_exists($item, $header)) {
                     $arr_tmp[] = $item->$header;
@@ -42,23 +44,14 @@ class BillExport implements FromCollection, WithHeadings, WithColumnWidths, With
 
             $res[] = $arr_tmp;
         }
-//        echo "<pre>";
-//        print_r($res);
-//        exit;
-        return collect($this->items);
+        return collect($res);
     }
 
     public function headings(): array {
-        return $this->headers;
+        return $this->headers_ko;
     }
 
     public function columnWidths(): array {
-//        $res = array();
-//        foreach ($this->headers as $header) {
-//            $res[$header] = 10;
-//        }
-//        return $res;
-//    }
         $res = [
             'A' => 25, 'B' => 10, 'C' => 30, 'D' => 30, 'E' => 20, 'F' => 10,
             'G' => 20, 'H' => 20, 'I' => 10, 'J' => 10, 'K' => 20, 'L' => 25,
@@ -68,9 +61,6 @@ class BillExport implements FromCollection, WithHeadings, WithColumnWidths, With
             'AE' => 10, 'AF' => 15, 'AG' => 20
        ];
 
-//        echo "<pre>";
-//        print_r($res);
-//        exit;
         return $res;
     }
 
